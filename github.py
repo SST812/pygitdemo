e1f3bcbcd95ad6cfb73072d0e2df8702c1c28766
SST812 <ssteja.812@gmail.com>
2023-03-23 22:45:56 +0530

    used git-update in pycharm

--- github.py
+++ github.py
@@ -3,3 +3,5 @@ print("This is written in git hub online")
 print("this is written in pycharm windows")
 
 print("after first two statements this is written in github website")
+
+print("the above print is updated if we use git - update project in pycharm")
