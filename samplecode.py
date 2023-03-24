dc65dcb377cc632dea0e85f8763314bebc4ea57c
SST812 <128595119+SST812@users.noreply.github.com>
2023-03-24 12:17:56 +0530

    verified merging

--- samplecode.py
+++ samplecode.py
@@ -5,3 +5,4 @@ print("ok good job, written in github")
 
 print("4th print statement was now completely changed in student and trying to merge with master")
 print("additional matter is properly merged, now changing the print statement it self to see how merge works")
+print("as a result the 4th statement from master is deleted and replaced by this 4th statement in student")
